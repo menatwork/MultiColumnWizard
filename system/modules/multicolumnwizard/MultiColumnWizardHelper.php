0f76cf383fbbee199c8c82030a375ef411986a79
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-01-14 12:26:17 +0100

    Trying to satisfy Psi

--- system/modules/multicolumnwizard/MultiColumnWizardHelper.php
+++ system/modules/multicolumnwizard/MultiColumnWizardHelper.php
@@ -47,7 +47,7 @@ class MultiColumnWizardHelper extends Backend
 
             if ($GLOBALS['TL_DCA'][$strTable]['fields'][$strField]['inputType'] == 'multiColumnWizard') {
 
-                $GLOBALS['TL_DCA'][$strTable]['fields'][$this->Input->get('field')] = $GLOBALS['TL_DCA'][$strTable]['fields'][$strField]['eval']['columnFields'][$strColumn];
+                $GLOBALS['TL_DCA'][$strTable]['fields'][$strField . '__' . $strColumn] = $GLOBALS['TL_DCA'][$strTable]['fields'][$strField]['eval']['columnFields'][$strColumn];
             }
         }
     }
