eda3e5157f12b4feedc2ca03f5079e9b01b77c96
David Maack <david.maack@arcor.de>
2014-04-04 12:12:33 +0200

    Added support for filePicker wizard.

--- system/modules/multicolumnwizard/MultiColumnWizardHelper.php
+++ system/modules/multicolumnwizard/MultiColumnWizardHelper.php
@@ -128,5 +128,16 @@ class MultiColumnWizardHelper extends System
             echo $objWidget->generate();
             exit; break;
         }
-    } 
+    }
+
+    /**
+     * Generates a filePicker icon for Contao Version > 3.1
+     * @param DataContainer $dc
+     *
+     * @return string
+     */
+    public function mcwFilePicker(DataContainer $dc)
+    {
+        return ' <a href="contao/file.php?do='.Input::get('do').'&amp;table='.$dc->table.'&amp;field='.preg_replace('/_row[0-9]*_/i', '__', $dc->field).'&amp;value='.$dc->value.'" title="'.specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MSC']['filepicker'])).'" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\''.specialchars($GLOBALS['TL_LANG']['MOD']['files'][0]).'\',\'url\':this.href,\'id\':\''.$dc->field.'\',\'tag\':\'ctrl_'.$dc->field . ((Input::get('act') == 'editAll') ? '_' . $dc->id : '').'\',\'self\':this});return false">' . Image::getHtml('pickfile.gif', $GLOBALS['TL_LANG']['MSC']['filepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
+    }
 }
