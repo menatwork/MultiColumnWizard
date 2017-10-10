8db8171d5454ce661e46b040584e6d75b9591f0e
Ingolf Steinhardt <info@e-spin.de>
2017-10-10 09:12:54 +0200

    hotfix add page.php to supportModalSelector

--- system/modules/multicolumnwizard/MultiColumnWizardHelper.php
+++ system/modules/multicolumnwizard/MultiColumnWizardHelper.php
@@ -15,6 +15,7 @@
  * Class MultiColumnWizardHelper
  *
  * @copyright  terminal42 gmbh 2013
+ * @author     Ingolf Steinhardt <info@e-spin.de> 2017
  * @package    MultiColumnWizard
  */
 class MultiColumnWizardHelper extends System
@@ -30,26 +31,30 @@ class MultiColumnWizardHelper extends System
     public function addScriptsAndStyles(&$objTemplate)
     {
         //do not allow version information to be leaked in the backend login and install tool (#184)
-        if ($objTemplate->getName() != 'be_login' && $objTemplate->getName() != 'be_install')
-        {
+        if ($objTemplate->getName() != 'be_login' && $objTemplate->getName() != 'be_install') {
             $GLOBALS['TL_JAVASCRIPT']['mcw'] = $GLOBALS['TL_CONFIG']['debugMode']
                 ? 'system/modules/multicolumnwizard/html/js/multicolumnwizard_be_src.js'
                 : 'system/modules/multicolumnwizard/html/js/multicolumnwizard_be.js';
             $GLOBALS['TL_CSS']['mcw']        = $GLOBALS['TL_CONFIG']['debugMode']
                 ? 'system/modules/multicolumnwizard/html/css/multicolumnwizard_src.css'
                 : 'system/modules/multicolumnwizard/html/css/multicolumnwizard.css';
-            $objTemplate->ua .= ' version_' . str_replace('.', '-', VERSION) . '-' . str_replace('.', '-', BUILD);
+            $objTemplate->ua                 .= ' version_' . str_replace('.', '-', VERSION) . '-' . str_replace(
+                    '.',
+                    '-',
+                    BUILD
+                );
         }
     }
 
     public function supportModalSelector($strTable)
     {
-        if (strpos(Environment::get('script'), 'contao/file.php') !== false)
-        {
+        if (strpos(Environment::get('script'), 'contao/file.php') !== false
+            || strpos(Environment::get('script'),'contao/page.php') !== false) {
             list($strField, $strColumn) = explode('__', $this->Input->get('field'));
 
             if ($GLOBALS['TL_DCA'][$strTable]['fields'][$strField]['inputType'] == 'multiColumnWizard') {
-                $GLOBALS['TL_DCA'][$strTable]['fields'][$strField . '__' . $strColumn] = $GLOBALS['TL_DCA'][$strTable]['fields'][$strField]['eval']['columnFields'][$strColumn];
+                $GLOBALS['TL_DCA'][$strTable]['fields'][$strField . '__' . $strColumn] =
+                    $GLOBALS['TL_DCA'][$strTable]['fields'][$strField]['eval']['columnFields'][$strColumn];
             }
         }
     }
@@ -59,22 +64,21 @@ class MultiColumnWizardHelper extends System
      */
     public function changeAjaxPostActions()
     {
-        if(version_compare(VERSION,'3.1', '>=')){
-            if (Environment::get('isAjaxRequest'))
-            {
-                switch (Input::post('action'))
-                {
+        if (version_compare(VERSION, '3.1', '>=')) {
+            if (Environment::get('isAjaxRequest')) {
+                switch (Input::post('action')) {
                     case 'reloadPagetree':
                     case 'reloadFiletree':
                         //get the fieldnames
-                        $strRef = $this->Session->get('filePickerRef');
-                        $strRef = substr($strRef, stripos($strRef, 'field=')+6);
-                        $arrRef = explode('&', $strRef);
+                        $strRef      = $this->Session->get('filePickerRef');
+                        $strRef      = substr($strRef, stripos($strRef, 'field=') + 6);
+                        $arrRef      = explode('&', $strRef);
                         $arrRefField = explode('__', $arrRef[0]);
-                        $arrField = preg_split('/_row[0-9]*_/i', \Input::post('name'));
+                        $arrField    = preg_split('/_row[0-9]*_/i', \Input::post('name'));
                         //change action if modal selector was found
-                        if (count($arrRefField) > 1 && $arrRefField === $arrField)
-                            Input::setPost('action', Input::post('action').'_mcw');
+                        if (count($arrRefField) > 1 && $arrRefField === $arrField) {
+                            Input::setPost('action', Input::post('action') . '_mcw');
+                        }
                         break;
                 }
             }
@@ -89,17 +93,16 @@ class MultiColumnWizardHelper extends System
      */
     public function executePostActions($action, \DataContainer $dc)
     {
-        if ($action == 'reloadFiletree_mcw' || $action == 'reloadPagetree_mcw' )
-        {
+        if ($action == 'reloadFiletree_mcw' || $action == 'reloadPagetree_mcw') {
             //get the fieldname
-            $strRef = $this->Session->get('filePickerRef');
-            $strRef = substr($strRef, stripos($strRef, 'field=')+6);
-            $arrRef = explode('&', $strRef);
+            $strRef   = $this->Session->get('filePickerRef');
+            $strRef   = substr($strRef, stripos($strRef, 'field=') + 6);
+            $arrRef   = explode('&', $strRef);
             $strField = $arrRef[0];
 
             //get value and fieldName
             $strFieldName = \Input::post('name');
-            $varValue = \Input::post('value');
+            $varValue     = \Input::post('value');
 
             //get the fieldname parts
             $arrfieldParts = preg_split('/_row[0-9]*_/i', $strFieldName);
@@ -107,28 +110,23 @@ class MultiColumnWizardHelper extends System
             $intRow = substr(substr($arrRow[0], 4), 0, -1);
 
             //build fieldname
-            $strFieldName = $arrfieldParts[0] . '[' . $intRow . '][' . $arrfieldParts[1] .']';
+            $strFieldName = $arrfieldParts[0] . '[' . $intRow . '][' . $arrfieldParts[1] . ']';
 
             $strKey = ($action == 'reloadPagetree_mcw') ? 'pageTree' : 'fileTree';
 
             // Convert the selected values
-            if ($varValue != '')
-            {
+            if ($varValue != '') {
                 $varValue = trimsplit("\t", $varValue);
 
                 // Automatically add resources to the DBAFS
-                if ($strKey == 'fileTree')
-                {
-                    if(version_compare(VERSION,'3.1', '>=') && version_compare(VERSION,'3.2', '<'))
-                    {
+                if ($strKey == 'fileTree') {
+                    if (version_compare(VERSION, '3.1', '>=') && version_compare(VERSION, '3.2', '<')) {
                         $fileId = 'id';
                     }
-                    if(version_compare(VERSION,'3.2', '>='))
-                    {
+                    if (version_compare(VERSION, '3.2', '>=')) {
                         $fileId = 'uuid';
                     }
-                    foreach ($varValue as $k=>$v)
-                    {
+                    foreach ($varValue as $k => $v) {
                         $varValue[$k] = \Dbafs::addResource($v)->$fileId;
                     }
                 }
@@ -136,9 +134,9 @@ class MultiColumnWizardHelper extends System
                 $varValue = serialize($varValue);
             }
 
-            $arrAttribs['id'] = \Input::post('name');
-            $arrAttribs['name'] = $strFieldName;
-            $arrAttribs['value'] = $varValue;
+            $arrAttribs['id']       = \Input::post('name');
+            $arrAttribs['name']     = $strFieldName;
+            $arrAttribs['value']    = $varValue;
             $arrAttribs['strTable'] = $dc->table;
             $arrAttribs['strField'] = $strField;
 
@@ -152,7 +150,7 @@ class MultiColumnWizardHelper extends System
                     ->execute(\Input::get('id'));
 
                 if ($stmt->numRows > 0) {
-                    $dc->activeRecord = $stmt;
+                    $dc->activeRecord         = $stmt;
                     $objWidget->dataContainer = $dc;
                 }
             }
@@ -164,12 +162,22 @@ class MultiColumnWizardHelper extends System
 
     /**
      * Generates a filePicker icon for Contao Version > 3.1
+     *
      * @param DataContainer $dc
      *
      * @return string
      */
     public function mcwFilePicker(DataContainer $dc)
     {
-        return ' <a href="contao/file.php?do='.Input::get('do').'&amp;table='.$dc->table.'&amp;field='.preg_replace('/_row[0-9]*_/i', '__', $dc->field).'&amp;value='.$dc->value.'" title="'.specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MSC']['filepicker'])).'" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\''.specialchars($GLOBALS['TL_LANG']['MOD']['files'][0]).'\',\'url\':this.href,\'id\':\''.$dc->field.'\',\'tag\':\'ctrl_'.$dc->field . ((Input::get('act') == 'editAll') ? '_' . $dc->id : '').'\',\'self\':this});return false">' . Image::getHtml('pickfile.gif', $GLOBALS['TL_LANG']['MSC']['filepicker'], 'style="vertical-align:top;cursor:pointer"') . '</a>';
+        return ' <a href="contao/file.php?do=' . Input::get('do') . '&amp;table=' . $dc->table . '&amp;field='
+               . preg_replace('/_row[0-9]*_/i', '__', $dc->field) . '&amp;value=' . $dc->value . '" title="'
+               . specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['MSC']['filepicker']))
+               . '" onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':765,\'title\':\''
+               . specialchars($GLOBALS['TL_LANG']['MOD']['files'][0]) . '\',\'url\':this.href,\'id\':\'' . $dc->field
+               . '\',\'tag\':\'ctrl_' . $dc->field
+               . ((Input::get('act') == 'editAll') ? '_' . $dc->id : '')
+               . '\',\'self\':this});return false">'
+               . Image::getHtml('pickfile.gif',$GLOBALS['TL_LANG']['MSC']['filepicker'],'style="vertical-align:top;cursor:pointer"')
+               . '</a>';
     }
 }
