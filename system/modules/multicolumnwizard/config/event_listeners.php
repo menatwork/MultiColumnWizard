471bc01bcd81bd0714f2c3bd03357d36f4f9afe9
Sven Baumann <baumann.sv@gmail.com>
2018-02-04 21:49:31 +0100

    Revert the update data definition listener

--- system/modules/multicolumnwizard/config/event_listeners.php
+++ system/modules/multicolumnwizard/config/event_listeners.php
@@ -4,30 +4,21 @@
  * Contao Open Source CMS
  *
  * @copyright  MEN AT WORK 2017
- * @author     Sven Baumann <baumann.sv@gmail.com> 2017
  * @package    MultiColumnWizard
  * @license    LGPL
  * @filesource
  */
 
-use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\BuildWidgetEvent;
-use ContaoCommunityAlliance\DcGeneral\DcGeneralEvents;
-use ContaoCommunityAlliance\DcGeneral\Factory\Event\PopulateEnvironmentEvent;
+use ContaoCommunityAlliance\DcGeneral\Factory\Event\BuildDataDefinitionEvent;
 use MultiColumnWizard\DcGeneral\UpdateDataDefinition;
 
-if (class_exists(DcGeneralEvents::class)) {
+if (class_exists(BuildDataDefinitionEvent::class)) {
     return array
     (
-        PopulateEnvironmentEvent::NAME => array(
+        BuildDataDefinitionEvent::NAME => array(
             array(
                 array(new UpdateDataDefinition(), 'addMcwFields'),
-                UpdateDataDefinition::POPULATE_PRIORITY
-            )
-        ),
-        BuildWidgetEvent::NAME => array(
-            array(
-                array(new UpdateDataDefinition(), 'setModalValue'),
-                UpdateDataDefinition::BUILD_WIDGET_PRIORITY
+                UpdateDataDefinition::PRIORITY
             )
         )
     );
--- system/modules/multicolumnwizard/src/MultiColumnWizard/DcGeneral/UpdateDataDefinition.php
+++ system/modules/multicolumnwizard/src/MultiColumnWizard/DcGeneral/UpdateDataDefinition.php
@@ -11,83 +11,31 @@
 
 namespace MultiColumnWizard\DcGeneral;
 
-use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\BuildWidgetEvent;
 use ContaoCommunityAlliance\DcGeneral\DataDefinition\Definition\Properties\DefaultProperty;
-use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
-use ContaoCommunityAlliance\DcGeneral\Factory\Event\PopulateEnvironmentEvent;
+use ContaoCommunityAlliance\DcGeneral\Factory\Event\BuildDataDefinitionEvent;
 
 /**
  * Class UpdateDataDefinition
  *
- * @author     Sven Baumann <baumann.sv@gmail.com> 2017
- * @package    MultiColumnWizard\DcGeneral
+ * @package MultiColumnWizard\DcGeneral
  */
 class UpdateDataDefinition
 {
-    const POPULATE_PRIORITY     = -500000;
-    const BUILD_WIDGET_PRIORITY = 500000;
+    const PRIORITY = -500000;
 
     /**
      * Add all fields from the MCW to the DCA. This is needed for some fields, because other components need this
      * to create the widget/view etc.
      *
-     * @param PopulateEnvironmentEvent $event
+     * @param BuildDataDefinitionEvent $event
      *
      * @return void
      */
-    public function addMcwFields(PopulateEnvironmentEvent $event)
+    function addMcwFields(BuildDataDefinitionEvent $event)
     {
-        $inputPropertyName = $this->getInputPropertyName($event->getEnvironment());
-
-        // If the property don´t find in post or get then do nothing. The property must only add for popups.
-        if (false === (bool) $inputPropertyName) {
-            return;
-        }
-
-        // Add the sub properties of MCW in modal view or in ajax call.
-        $this->buildProperty($inputPropertyName, $event->getEnvironment());
-    }
-
-    /**
-     * Set the used modal value.
-     * By Widget::getPost the post key split in parts, then don`t find the return value from modal.
-     *
-     * @param BuildWidgetEvent $event The event.
-     *
-     * @return void
-     */
-    public function setModalValue(BuildWidgetEvent $event)
-    {
-        $environment    = $event->getEnvironment();
-        $sessionStorage = $environment->getSessionStorage();
-        $session        = (array) $sessionStorage->get('MCW_MODAL_UPDATE');
-
-        if (false === isset($session[md5($event->getProperty()->getName())])) {
-            return;
-        }
-
-        $input = $environment->getInputProvider();
-        $data = $session[md5($event->getProperty()->getName())];
-
-        $input->setValue($data['key'], $input->getValue($data['valueFrom']));
-
-        unset($session[md5($event->getProperty()->getName())]);
-        $sessionStorage->set('MCW_MODAL_UPDATE', $session);
-    }
-
-    /**
-     * Build the MCW column property and add to properties.
-     *
-     * @param string               $propertyName The property name.
-     *
-     * @param EnvironmentInterface $environment  The environment.
-     *
-     * @return void
-     */
-    private function buildProperty($propertyName, EnvironmentInterface $environment)
-    {
-        $dataDefinition = $environment->getDataDefinition();
-        $properties     = $dataDefinition->getPropertiesDefinition();
+        // Get the container and all properties.
+        $container = $event->getContainer();
+        $properties = $container->getPropertiesDefinition();
 
         /** @var DefaultProperty $property */
         foreach ($properties as $property) {
@@ -100,20 +48,16 @@ class UpdateDataDefinition
             $config = $property->getExtra();
 
             // If we have no data here, go to the next.
-            if (empty($config['columnFields']) || !is_array($config['columnFields'])) {
+            if(empty($config['columnFields']) || !is_array($config['columnFields'])){
                 continue;
             }
 
             foreach ($config['columnFields'] as $fieldKey => $fieldConfig) {
-                if ((true === $properties->hasProperty($propertyName))
-                    || (false === strpos($propertyName, $property->getName()))
-                    || ('[' . $fieldKey . ']' !== substr($propertyName, -strlen('[' . $fieldKey . ']')))
-                ) {
-                    continue;
-                }
+                // Build the default name.
+                $name = sprintf('%s__%s', $property->getName(), $fieldKey);
 
                 // Make a new field and fill it with the data from the config.
-                $subProperty = new DefaultProperty($propertyName);
+                $subProperty = new DefaultProperty($name);
                 foreach ($fieldConfig as $key => $value) {
                     switch ($key) {
                         case 'label':
@@ -133,15 +77,15 @@ class UpdateDataDefinition
                             break;
 
                         case 'exclude':
-                            $subProperty->setExcluded((bool) $value);
+                            $subProperty->setExcluded((bool)$value);
                             break;
 
                         case 'search':
-                            $subProperty->setSearchable((bool) $value);
+                            $subProperty->setSearchable((bool)$value);
                             break;
 
                         case 'filter':
-                            $subProperty->setFilterable((bool) $value);
+                            $subProperty->setFilterable((bool)$value);
                             break;
 
                         case 'inputType':
@@ -178,57 +122,10 @@ class UpdateDataDefinition
                     }
                 }
 
+                // Add all to the current list.
                 $properties->addProperty($subProperty);
-
-                $subExtra = $subProperty->getExtra();
-
-                // Fore some widgets must declare the evaluation in the data container configuration.
-                $GLOBALS['TL_DCA'][$dataDefinition->getName()]['fields'][$subProperty->getName()]['eval'] = $subExtra;
             }
         }
-    }
 
-    /**
-     * Get the input property from input.
-     *
-     * @param EnvironmentInterface $environment The environment.
-     *
-     * @return string
-     */
-    private function getInputPropertyName(EnvironmentInterface $environment)
-    {
-        $input = $environment->getInputProvider();
-
-        if ((true === $input->hasParameter('field'))
-            && (false === count($input->getParameter('field')))
-        ) {
-            return $input->getParameter('field');
-        }
-        if ((true === $input->hasValue('name'))
-            && (false === count($input->getValue('name')))
-        ) {
-            $sessionStorage = $environment->getSessionStorage();
-            $session        = $sessionStorage->get('MCW_MODAL_UPDATE');
-
-            $chunks = explode(
-                '::::',
-                str_replace(
-                    array('][', '[', ']'),
-                    array('::::', '::::', ''),
-                    $input->getValue('name')
-                )
-            );
-
-            $session[md5($input->getValue('name'))] = array(
-                'key'       => array_shift($chunks),
-                'valueFrom' => $input->getValue('name')
-            );
-
-            $sessionStorage->set('MCW_MODAL_UPDATE', $session);
-
-            return $input->getValue('name');
-        }
-
-        return '';
     }
 }
